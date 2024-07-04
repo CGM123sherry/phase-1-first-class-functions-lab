66e585b0f12ebbebff189eefc4e4ab4065f550a7
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-04 06:10:03 +0300

    first-class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+//slicing the first two drivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  //return the last two drivers
+  const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+  }
+  //selecting drivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  //creating a fare multiplier
+  function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    };
+  }
+  //fare doubler
+    function fareDoubler(fare) {
+        const doubler = createFareMultiplier(2);
+        return doubler(fare);
+      }
+      // fare tripler
+      function fareTripler(fare){
+        const tripler = createFareMultiplier(3);
+        return tripler(fare);
+      }
+      //selecting drivers
+      function selectDifferentDrivers(drivers, driverSelector) {
+        return driverSelector(drivers);
+      }
